7d0ca55dd6bb7d56a1b314afe9f65188d762e559
Reyberry <87958451+Reyberry@users.noreply.github.com>
2021-07-30 08:35:16 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,27 @@
+var seaImg;
+var shipImg1;
 
 function preload(){
-
+seaImg = loadImage("sea.png");
+shipImg1=loadAnimation("ship-1.png");
 }
-
 function setup(){
-  createCanvas(400,400);
-  
+ createCanvas (600,600);
+ sea=createSprite(50,50);
+ sea.addImage("sea",seaImg);
+ //sea.x=sea.width/2;
+ ship = createSprite(200,200);
+ ship.addAnimation("running",shipImg1);
+ ship.scale=0.25;
+ ship.x=100;
+ 
+  drawSprites();
 }
 
-function draw() {
-  background("blue");
- 
+function draw(){
+ sea.velocityX = -2;
+ console.log(sea.x);
+ if (sea.x < 0){
+ sea.x = sea.width/2;
+ }
 }
